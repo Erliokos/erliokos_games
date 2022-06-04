7a39a871971b2a3bfec636b191df9582f48daa17
Альберт Мурзаков <albertmurzakov@MacBook-Pro-Albert.local>
2022-06-05 01:30:20 +0300

    create-react-app

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import * as Styled from './Style';
+
+export const Main = () => {
+  return (
+    <Styled.Header>
+      
+    </Styled.Header>
+  )
+}
+
---
+++ src/components/Main/Style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Header = styled.div`
+  background-color: lightsteelblue;
+  width: 100vw;
+  height: 100vh;
+`;
+
